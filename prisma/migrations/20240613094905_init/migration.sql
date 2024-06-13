d9ce5de2335c9421e047785d290c0ea50fa0c8a9
safir72347 <safirmotiwala@gmail.com>
2024-06-13 15:22:12 +0530

    Added prisma for postgres ORM

---
+++ prisma/migrations/20240613094905_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "LinkPrecedence" AS ENUM ('secondary', 'primary');
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "phoneNumber" TEXT,
+    "email" TEXT,
+    "linkedId" INTEGER,
+    "linkPrecedence" "LinkPrecedence" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contact_linkedId_key" ON "Contact"("linkedId");
