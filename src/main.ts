a37026f13412ae1ee45e97a1e29253a4ed4f0d02
safir72347 <safirmotiwala@gmail.com>
2024-06-13 15:44:40 +0530

    Fix packages and finalizing the env

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -25,6 +26,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000).then(() => {
+    console.log(`⚡ Server listening on port ${process.env.PORT || 3000}`);
+  });
 }
 bootstrap();
