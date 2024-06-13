94bce5da86aceb004021d93da1f2f12a98fee833
safir72347 <safirmotiwala@gmail.com>
2024-06-13 15:27:36 +0530

    Removing boiler plate code and adding prisma module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super();
+  }
+  async onModuleDestroy() {
+    await this.$connect;
+  }
+  async onModuleInit() {
+    await this.$disconnect;
+  }
+}
